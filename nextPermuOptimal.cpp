8d38613120accc17bc48a783083e22200f2ba230
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-04 15:20:05 +0530

    Create nextPermuOptimal.cpp

---
+++ nextPermuOptimal.cpp
@@ -0,0 +1,2 @@
+//we wont do brute force here.. i will find the video and use recursion to generate all permutations later
+//there is an stl function in c++ which we can use to find the next permutation
