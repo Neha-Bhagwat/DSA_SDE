ea7007ceb2d29e0c8dbd9a7380131ab7873c5b7b
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-04 15:27:41 +0530

    Create maxSubArrayBf.cpp

---
+++ maxSubArrayBf.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int sum, len = nums.size(), maxi;
+for(int i = 0; i < len;i++)
+{
+  sum = 0;
+  for(int j = 0; j < len; j++)
+  {
+    sum =+nums[i];
+    maxi = max(sum, maxi);
+  }
+}  
