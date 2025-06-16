f3bd74bcaf91a0706769f6d196b2dc0cbd31c185
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-16 22:46:55 +0530

    Update findDuplicateBF.cpp

--- findDuplicateBF.cpp
+++ findDuplicateBF.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        //BF solution is to make a count array to store the number of times a number comes n the list
+        //BF solution is to make a count array to store the number of times a number comes n the list. This method is called Hashing. we will make a new array of size 10 and update the count of each element. when the count becomes 2, we output that one as the element that is repeated.
         //or BF soln can be to sort the array and then check
         //since this is constant extra space, maybe we can use 2 ptrs and check them like youd do in bubble sort. So i will write that code rn 
         int answer = 0;       
