41125942de07884361b246d5b58e0a0505dc14b2
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-16 11:49:43 +0530

    Update mergeIntervalBF.cpp

--- mergeIntervalBF.cpp
+++ mergeIntervalBF.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         //brute force solution. first sort intervals
-        for(int i = 0; i < intervals.size() - 1; i++)
-        {
-            for (int j = 0; j< intervals.size() - 1; j++)
-            {
-                if(intervals[j][0] > intervals[j+1][0])
-                {
-                    swap(intervals[j], intervals[j+1]);
-                }
-                else if (intervals[j][0] == intervals[j+1][0])
-                {
-                    if(intervals[j][1] > intervals[j+1][1])
-                    {
-                        swap(intervals[j], intervals[j+1]);
-                    }
-                }
-            }
-        }
+        // for(int i = 0; i < intervals.size() - 1; i++)
+        // {
+        //     for (int j = 0; j< intervals.size() - 1; j++)
+        //     {
+        //         if(intervals[j][0] > intervals[j+1][0])
+        //         {
+        //             swap(intervals[j], intervals[j+1]);
+        //         }
+        //         else if (intervals[j][0] == intervals[j+1][0])
+        //         {
+        //             if(intervals[j][1] > intervals[j+1][1])
+        //             {
+        //                 swap(intervals[j], intervals[j+1]);
+        //             }
+        //         }
+        //     }
+        // }
 
         //or better, use the sort operator
+        sort(intervals.begin() , intervals.end());
+
         //then club intervals
         for(int i = 0; i < intervals.size()- 1; i++)
         {
