f9a704ba11c631620da5a994051194b4f5fe3a3d
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-17 17:20:35 +0530

    Update and rename noOfInversions.cpp to noOfInversionsBubbleBF.cpp

--- noOfInversionsBubbleBF.cpp
+++ noOfInversionsBubbleBF.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
    long long int numberOfInversions(vector<int> nums) {
+      //this uses bubble sort algo repurposed for counting inversions
         long long int n = nums.size();
         long long int noInversion = 0;
         for(long long int i = 0; i < n ; i ++)
         {
             for ( long long int j = i; j<n; j++)
             {
-                if(nums[i] > nums[j])
+                if(nums[i] > nums[j]) //checking condition for inversion
                 {
-                    noInversion++;
+                    noInversion++; //incrementing counter
                 }
             }
         }
