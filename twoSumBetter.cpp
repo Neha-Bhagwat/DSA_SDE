6aee6c4ec6689933e8c79666e1e6cab546aa117f
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-06-23 20:26:45 +0530

    Create twoSumBetter.cpp

---
+++ twoSumBetter.cpp
@@ -0,0 +1,21 @@
+//this solution uses hashmaps. if no number matches target-num inside the hashmap, you add number to the hashmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for (int i =0; i < n; i++)    
+        {
+            //int a = nums[i];
+            int more = target - nums[i];
+            //check if more is in the map
+            if(mpp.find(more) != mpp.end()) //if you dont find it itll go to the end
+            {
+                //mpp[more] gives the index
+                return {mpp[more],i};
+            }
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
