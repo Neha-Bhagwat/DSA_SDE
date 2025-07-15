08ae65a606e9b971e18815c3900181eae8bff636
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-07-15 11:07:18 +0530

    Update fiboTabu.cpp

--- DynamicProgramming/fiboTabu.cpp
+++ DynamicProgramming/fiboTabu.cpp
@@ -3,10 +3,9 @@ public:
     int fib(int n) 
     {
         vector<int> dp(n+1, -1);
+        if(n <= 1) return n;
         dp[0] = 0;
-        if(n == 0) return dp[n];
         dp[1] = 1;
-        if(n == 1) return dp[n];
         for(int i = 2; i<=n; i++)
         {
             dp[i] = dp[i-1]+dp[i-2];
