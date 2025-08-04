3421cd9968b28f0dd9b3c856c2a86934ca864ff5
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-08-04 15:47:41 +0530

    Update palindromePartitioning.cpp

--- Recursion/palindromePartitioning.cpp
+++ Recursion/palindromePartitioning.cpp
@@ -1,3 +1,14 @@
+//Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+// Example 1:
+
+// Input: s = "aab"
+// Output: [["a","a","b"],["aa","b"]]
+// Example 2:
+
+// Input: s = "a"
+// Output: [["a"]]
+
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
