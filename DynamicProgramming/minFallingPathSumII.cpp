4ff55f25f014c167823f591a4058687225b64b3e
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-08-09 00:11:45 +0530

    Update and rename minPathII.cpp to minFallingPathSumII.cpp

--- DynamicProgramming/minFallingPathSumII.cpp
+++ DynamicProgramming/minFallingPathSumII.cpp
@@ -1,3 +1,7 @@
+//Q. Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+//A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two elements chosen in adjacent rows are in the same column.
+
+
 class Solution {
 public:
     // int helper(int idx, int col, vector<vector<int>>& grid, vector<vector<int>>& dp)
