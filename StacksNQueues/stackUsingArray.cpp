2dde5212ee6bb9e0b39303f3ee56d9363a880115
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2025-08-16 18:02:46 +0530

    Create stackUsingArray.cpp

---
+++ StacksNQueues/stackUsingArray.cpp
@@ -0,0 +1,43 @@
+//Thoda gay sum
+
+// Write a program to implement a stack using an array oper[]. You need  to complete the push(int x) and pop() methods inside a class MyStack to simulate the standard stack operations:
+// push(x): Pushes the integer x onto the stack.
+// pop(): Removes and returns the topmost element of the stack. If the stack is empty, return -1.
+// You will be given a list of space-separated queries consisting of two types:
+// Type 1 : 1 x — Push x into the stack.
+// Type 2 : 2  — Pop the top element from the stack and print it. If the stack is empty, print -1.
+// Note: It is guaranteed that for Type 1, there will always be a value x.
+// Examples :
+
+// Input: oper[] = [1, 2, 1, 3, 2, 1, 4, 2] 
+// Output: [3, 4]
+// Explanation: 
+// push(2)   the stack will be {2}
+// push(3)   the stack will be {2 3}
+// pop()     poped element will be 3, the stack will be {2}
+// push(4)   the stack will be {2 4}
+// pop()     poped element will be 4
+// Input: oper[] = [2, 1, 4, 1, 5, 2]
+// Output: [-1, 5]
+
+class MyStack {
+  public:
+    int arr[100];
+    int top = -1;
+    int i = 0;
+    void push(int x) {
+        arr[i] = x;
+        top = x;
+        i++;
+    }
+
+    int pop() {
+        if(i == 0)
+        {
+            top = -1;
+            return -1;
+        }
+        i--;
+        return top = arr[i];
+    }
+};
