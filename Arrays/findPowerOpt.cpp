d415cbea8c1ccf6fb0def6de7f3f6a1b2fb10dd7
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2026-06-03 18:55:05 +0530

    Update findPowerOpt.cpp

--- Arrays/findPowerOpt.cpp
+++ Arrays/findPowerOpt.cpp
@@ -1,7 +1,22 @@
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+// Example 1:
+
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
+// Example 2:
+
+// Input: x = 2.10000, n = 3
+// Output: 9.26100
+// Example 3:
+
+// Input: x = 2.00000, n = -2
+// Output: 0.25000
+// Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
 class Solution {
 public:
     double myPow(double x, int n) {
-        //the logic here is that, if we have a even power, we do (x^2)^n/2. if odd, then prod = prod*(x) and then you get an even power that you have to compute
+        //the logic here is that, if we have a even power, we do (x^2)^n/2. if odd, then prod = prod*(x) and pow = pow-1 and then you get an even power that you have to compute
         double product = 1;
         //now, we make a copy of n and make it positive
         long long power = n;
@@ -17,7 +32,7 @@ public:
             else
             {
                 product = product*x;
-                power = (power-1);
+                power = power-1;
             }
         }
         if(n<0)
