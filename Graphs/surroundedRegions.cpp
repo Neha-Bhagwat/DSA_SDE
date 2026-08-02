14cd54b81762df0eb92b33c2a571c7c221bf8c04
Neha-Bhagwat <neha.v.bhagwat@gmail.com>
2026-08-02 23:32:06 +0530

    Add DFS solution for surrounded regions problem
    Implement DFS to solve the surrounded regions problem by marking 'O's connected to borders and changing unvisited 'O's to 'X'.
    Leetcode 130

---
+++ Graphs/surroundedRegions.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, vector<vector<int>>& vis, int row, int col)
+    {
+        // visit the node
+        int m = board.size(), n = board[0].size();
+        vis[row][col] = 1;
+        // go to neighbours
+        vector<int> drow = {-1, 1, 0, 0};
+        vector<int> dcol = {0, 0, -1, 1};
+        vector<int> nrow(4,0);
+        vector<int> ncol(4,0);
+        // define the row and col
+        for(int i = 0; i < 4; i++)
+        {
+            nrow[i] = drow[i] + row;
+            ncol[i] = dcol[i] + col;
+        }
+        for(int i = 0; i < 4; i++)
+        {
+            // run dfs on each if
+            // they arent out of bounds and they are O
+            int newRow = nrow[i], newCol = ncol[i];
+            if(newRow >= 0 && newCol >= 0 && newRow < m  && newCol < n && !vis[newRow][newCol] && board[newRow][newCol] == 'O')
+            {
+                dfs(board, vis, newRow, newCol);
+            }
+        }
+        
+    }
+    void solve(vector<vector<char>>& board) {
+        // make vis matrix
+        int m = board.size(), n = board[0].size();
+
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j  = 0; j < n; j++)
+            {
+                bool border = false;
+                // mark all xs as 1
+                if(board[i][j] == 'X') 
+                {
+                    vis[i][j] = 1;
+                }
+                // run dfs from all boundary Os (add vis there itself)
+                else if(i == 0 || i == m-1 || j == 0 || j == n-1) 
+                {
+                    border = true;
+                }
+                if(board[i][j] == 'O' && border == true && !vis[i][j]) dfs(board, vis, i, j);
+            }
+        }
+
+        // if O and not vis, change to X
+        for(int i = 0; i < m; i++)
+        {
+            for(int j  = 0; j < n; j++)
+            {
+                if(board[i][j] == 'O' && !vis[i][j]) board[i][j] = 'X';
+            }
+
+        }
+    }
+};
